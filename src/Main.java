db1fc63e2c5904d36b71dd5c7add9d14e8fe0e8d
User <nazarovtolya2@gmail.com>
2023-03-02 15:43:17 +0500

    Начало ДЗ

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
