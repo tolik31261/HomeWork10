971584b05a9208999deb7e1114efeeade400ec4b
User <nazarovtolya2@gmail.com>
2023-03-02 16:16:46 +0500

    Задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+        task3();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -10,4 +12,17 @@ public class Main {
         String fullName = firstName + " " + middleName + " " + lastName;
         System.out.println(fullName);
     }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника - " + fullName.replace('ё', 'е'));
+    }
+
 }
